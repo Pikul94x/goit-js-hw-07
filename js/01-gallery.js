017d86345c2be1b0fd107db3d3ea525d770c7cd7
Pikul94 <92268463+Pikul94@users.noreply.github.com>
2022-05-21 15:31:20 +0200

    initial commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
+const gallery = document.querySelector(".gallery");
 
 console.log(galleryItems);
+
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" data-source="${image.original}" src="${image.preview}" alt="${image.description}"></a></li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+const liItems = document.querySelectorAll(".gallery__link");
+
+liItems.forEach((item) => {
+  const lightbox = basicLightbox.create(
+    `
+		<img width="1400" height="900" src='${item.href}'>
+	`
+  );
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+    lightbox.show();
+  });
+  document.addEventListener("keydown", (e) => {
+    if (e.key === "Escape") {
+      lightbox.close();
+    }
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>`
+  )
+
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+
+const liItems = document.querySelectorAll(".gallery__link");
+
+liItems.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
+
+var lightbox = new SimpleLightbox(".gallery__item", {
+  captionsData: "alt",
+  captionDelay: 250
+});
