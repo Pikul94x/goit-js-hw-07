edde25740356ed9ea4a0de81edefb930a299e544
Pikul94 <92268463+Pikul94@users.noreply.github.com>
2022-06-01 20:20:38 +0200

    Update hw 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,31 +1,55 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+// console.log(galleryItems);
+
 const gallery = document.querySelector(".gallery");
 
 const markup = galleryItems
-  .map(
-    (image) =>
-      `<li class="gallery__item"><a class="gallery__link" href="${image.original}"><img class="gallery__image" data-source="${image.original}" src="${image.preview}" alt="${image.description}"></a></li>`
-  )
-  .join("");
+	.map(
+		item => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`
+	)
+	.join("");
 
 gallery.insertAdjacentHTML("beforeend", markup);
 
-const liItems = document.querySelectorAll(".gallery__link");
+gallery.addEventListener("click", e => {
+	e.preventDefault();
 
-liItems.forEach((item) => {
-  const lightbox = basicLightbox.create(
-    `
-		<img width="1400" height="900" src='${item.href}'>
-	`
-  );
-  item.addEventListener("click", (e) => {
-    e.preventDefault();
-    lightbox.show();
-  });
-  document.addEventListener("keydown", (e) => {
-    if (e.key === "Escape") {
-      lightbox.close();
-    }
-  });
+	if (e.target.nodeName !== "IMG") return;
+
+	const instance = basicLightbox.create(`
+    <img src="${e.target.dataset.source}" width="800" height="600">
+`);
+
+	instance.show();
+
+	document.addEventListener("keydown", e => {
+		if (e.key === "Escape") {
+			instance.close();
+		}
+	});
 });
+
+// // oneClick function
+
+// gallery.onclick = (e) => {
+//     e.preventDefault()
+
+// 	basicLightbox
+// 		.create(
+// 			`
+// 		<img width="1400" height="900" src="${e.target.dataset.source}">
+// 	`
+// 		)
+// 		.show();
+// };
